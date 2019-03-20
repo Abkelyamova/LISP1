43c913ebe8ca2c2d36a49b5475058c20079e85c9
Abkelyamova <43819580+Abkelyamova@users.noreply.github.com>
2019-03-21 02:54:44 +0300

    Update Task_38.lisp

--- Task_38.lisp
+++ Task_38.lisp
@@ -1,4 +1,13 @@
+
 ;38. Определите функцию ОБЪЕДИНЕНИЕ, формирующую объединение двух множеств.
+ 
+(defun num (x set)
+    (cond
+        ((null set) NIL)
+        ((equal (car set) x) t)
+        (t (num x (cdr set)))
+    )
+)
 
 (defun union-set (set1 set2)
     (cond 
@@ -6,7 +15,7 @@
         ((null set2) set1)
         (t ((lambda (head tail) 
                 (if 
-                    (member head set2) 
+                    (num head set2) 
                     (cons head (union-set tail (remove head set2)))
                     (cons head (union-set tail set2))
                 )
@@ -17,8 +26,6 @@
         )
     )
 )
- 
-
 (print (union-set '(a b c d) '(c d f a))); (a b c d f)
+
 (print (union-set '() '(a b c))); (a b c)
- 
