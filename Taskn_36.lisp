91c60093dddfca40164bbe534ebb9cf2878ee222
Abkelyamova <43819580+Abkelyamova@users.noreply.github.com>
2019-03-21 10:28:24 +0300

    Update Taskn_36.lisp

--- Taskn_36.lisp
+++ Taskn_36.lisp
@@ -1,13 +1,24 @@
-;36. Определите предикат НЕПЕРЕСЕКАЮЩИЕСЯ, проверяющий, что два множества не пересекаются, т.е. у них нет общих элементов.
+;36. Определите предикат НЕПЕРЕСЕКАЮЩИЕСЯ, проверяющий, 
+;что два множества не пересекаются, т.е. у них нет общих элементов.
 
-(defun in_dependet (set1 set2)
-    (cond 
-        ((null set1) t)
-        ((null (member (car set1) set2)) (in_dependet (cdr set1) set2))
+(defun memberp (x set)
+    (cond
+        ((null set) NIL)
+        ((equal (car set) x) t)
+        (t (memberp x (cdr set)))
     )
 )
 
-(print (in_dependet '(7 8 9) '(1 2 3 4 5))) ; T
-(print (in_dependet '(5 6 9 3) '(8 9 3 0 6))) ; nil
-(print (in_dependet '(a v) '(a b c d))) ; nil
-(print (in_dependet '(k) '(a b f c d))) ; T
+(defun not_intersect (set1 set2)
+    (cond 
+        ((null set1) t )
+        ((null(memberp (car set1) set2)) 
+              (not_intersect (cdr set1) set2)
+        )
+    )
+)
+
+(print (not_intersect '(7 8 9) '(1 2 3 4 5))) ; T
+(print (not_intersect '(5 6 9 3) '(8 9 3 0 6))) ; nil
+(print (not_intersect '(a v) '(a b c d))) ; nil
+(print (not_intersect '(k) '(a b f c d))) ; T
