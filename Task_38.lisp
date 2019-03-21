5ddb0fa6d758837e2f3564a33c5f91dc4d15b79b
Abkelyamova <43819580+Abkelyamova@users.noreply.github.com>
2019-03-21 10:13:10 +0300

    Update Task_38.lisp

--- Task_38.lisp
+++ Task_38.lisp
@@ -1,23 +1,42 @@
-
 ;38. Определите функцию ОБЪЕДИНЕНИЕ, формирующую объединение двух множеств.
  
-(defun num (x set)
+(defun removep(x set)
     (cond
-        ((null set) NIL)
-        ((equal (car set) x) t)
-        (t (num x (cdr set)))
+        ((null set) nil)
+        (t ((lambda (f rem)
+               (if (equal x f) 
+                   rem
+                  (cons f rem)
+               ) 
+            ) 
+            (car set)
+            (removep x (cdr set))
+           ) 
+        )
     )
 )
 
-(defun union-set (set1 set2)
+(defun memberp (x set)
+    (cond
+        ((null set) NIL)
+        ((equal (car set) x) t)
+        (t (memberp x (cdr set)))
+    )
+)
+
+(defun union_set (set1 set2)
     (cond 
         ((null set1) set2)
         ((null set2) set1)
         (t ((lambda (head tail) 
                 (if 
-                    (num head set2) 
-                    (cons head (union-set tail (remove head set2)))
-                    (cons head (union-set tail set2))
+                    (memberp head set2) 
+                    (cons head (union_set tail 
+                                         (removep head 
+                                                  set2)
+                                )
+                    )
+                    (cons head (union_set tail set2))
                 )
             )
             (car set1)
@@ -26,6 +45,7 @@
         )
     )
 )
-(print (union-set '(a b c d) '(c d f a))); (a b c d f)
 
-(print (union-set '() '(a b c))); (a b c)
+
+(print (union_set '(a b c d) '(c d f a))); (a b c d f)
+(print (union_set '() '(a b c))); (a b c)
