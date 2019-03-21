6434362e7f66ff397748dcc0638c8b3990601cca
Abkelyamova <43819580+Abkelyamova@users.noreply.github.com>
2019-03-21 09:10:17 +0300

    Update Task_16.lisp

--- Task_16.lisp
+++ Task_16.lisp
@@ -1,12 +1,17 @@
-;Определите функцию, добавляющую элементы одного списка во второй список, начиная с заданной позиции
+;16 Определите функцию, добавляющую элементы одного списка во второй список, 
+;начиная с заданной позиции
 
-(defun Gluing (list1 list2 i) 
+(defun insertp (list1 list2 i) 
     (cond ((null list1) list2)
-           ((null list2) list1))
- 
-        (if (equal i 0) (append list2 list1)
-         (cons (car list1) ( Gluing (cdr list1) list2 (- i 1))))
-        )
+          ((null list2) list1)
+          ((equal i 0) (append list2 list1))
+          (t (cons (car list1)
+                   (insertp (cdr list1) list2
+                   (- i 1))
+             )
+          )
+     )
+)
    
-(print(Gluing '(a b c d f g) '(0 0 0) 2)) ;(A B 0 0 0 C D F G) 
-(print(Gluing '(1 3 5) '(2 4 6) 0)); (2 4 6 1 3 5) 
+(print(insertp '(a b c d f g) '(0 0 0) 2)) ;(A B 0 0 0 C D F G) 
+(print(insertp '(1 3 5) '(2 4 6) 0)); (2 4 6 1 3 5) 
