7a6e739b9718bc93a033b77a7491636c588bef0c
Abkelyamova <43819580+Abkelyamova@users.noreply.github.com>
2019-03-28 07:24:21 +0200

    Update Task_38.lisp

--- Task_38.lisp
+++ Task_38.lisp
@@ -24,19 +24,19 @@
     )
 )
 
-(defun union_set (set1 set2)
+(defun union-set (set1 set2)
     (cond 
         ((null set1) set2)
         ((null set2) set1)
         (t ((lambda (head tail) 
                 (if 
                     (memberp head set2) 
-                    (cons head (union_set tail 
+                    (cons head (union-set tail 
                                          (removep head 
                                                   set2)
                                 )
                     )
-                    (cons head (union_set tail set2))
+                    (cons head (union-set tail set2))
                 )
             )
             (car set1)
@@ -47,5 +47,5 @@
 )
 
 
-(print (union_set '(a b c d) '(c d f a))); (a b c d f)
-(print (union_set '() '(a b c))); (a b c)
+(print (union-set '(a b c d) '(c d f a))); (a b c d f)
+(print (union-set '() '(a b c))); (a b c)
