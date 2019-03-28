073eb68a7d51987ee51de892290c47fc27c3d3e5
Abkelyamova <43819580+Abkelyamova@users.noreply.github.com>
2019-03-28 11:08:23 +0300

    Update Taskn_36.lisp

--- Taskn_36.lisp
+++ Taskn_36.lisp
@@ -11,14 +11,20 @@
 
 (defun not-intersect (set1 set2)
     (cond 
-        ((null set1) t )
-        ((null(memberp (car set1) set2)) 
+        ((null set1) t)
+        (t(if (memberp (car set1) set2 )
+               nil
               (not-intersect (cdr set1) set2)
+          )
         )
     )
 )
+    
+
 
 (print (not-intersect '(7 8 9) '(1 2 3 4 5))) ; T
 (print (not-intersect '(5 6 9 3) '(8 9 3 0 6))) ; nil
 (print (not-intersect '(a v) '(a b c d))) ; nil
 (print (not-intersect '(k) '(a b f c d))) ; T
+
+
