14042ecc27f3dbcc50d856161aef521422daeac8
Abkelyamova <43819580+Abkelyamova@users.noreply.github.com>
2019-04-03 23:27:29 +0300

    Update Task_35.lisp

--- Task_35.lisp
+++ Task_35.lisp
@@ -8,8 +8,15 @@
         (t (memberp x (cdr set)))
     )
 )
-    
+
 (defun subset (set1 set2)
+    (cond 
+        ((null set1) t)
+        ((memberp (car set1) set2) (subset (cdr set1) set2))
+    )
+)
+
+(defun own-subset (set1 set2)
     (cond 
         ((null set1) t)
         ((equalp set1 set2) nil)
@@ -18,8 +25,7 @@
 )
 
 (print (subset '(4 3) '(4 3))) ; T
+(print (own-subset '(4 3) '(4 3))) ; nil
 (print (subset '(5 6 9 3) '(8 9 3 0 6))) ; nil
-
 (print (subset '(a v) '(a b c d))) ; nil
 (print (subset '(a d f) '(a b f c d))) ; T
- 
