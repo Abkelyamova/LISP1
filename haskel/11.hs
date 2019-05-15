541c7e6a52fa824a38860c23ca80968bddf5b4a4
Abkelyamova <43819580+Abkelyamova@users.noreply.github.com>
2019-05-15 22:38:21 +0300

    Create 11.hs

---
+++ haskel/11.hs
@@ -0,0 +1,6 @@
+-- 11. Реализовать на языке Haskell функцию, которая, чередует элементы списков [a, b, ...] и [1, 2, ...] 
+
+mixing_lists [] [] = []
+mixing_lists (x:xs) (y:ys) = x:y:(mixing_lists xs ys)
+
+main = print $ (mixing_lists [1, 3, 5] [2, 4, 6]) -- [1,2,3,4,5,6]
