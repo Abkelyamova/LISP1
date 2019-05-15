66a9509ad743ab329829e8dd6fee3245240ad76e
Abkelyamova <43819580+Abkelyamova@users.noreply.github.com>
2019-05-15 19:19:25 +0300

    Create 05.hs

---
+++ haskel/05.hs
@@ -0,0 +1,10 @@
+-- 5. Реалиpовать на языке Haskell функцию, 
+-- замененящую в исходном списке два подряд идущих одинаковых элемента одним
+
+delete_couple :: [Integer] -> [Integer]
+delete_couple []         = []
+delete_couple (x:xs:xss) = if x == xs
+                           then x:(delete_couple xss)
+                           else x:(delete_couple (xs:xss))
+
+main = print $ delete_couple [1,1,4,5,5] -- [1,4,5]
