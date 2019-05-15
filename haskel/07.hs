8925b9bbbc9770b3eab37b617e9124ec2e405861
Abkelyamova <43819580+Abkelyamova@users.noreply.github.com>
2019-05-15 22:41:02 +0300

    Update 07.hs

--- haskel/07.hs
+++ haskel/07.hs
@@ -1,9 +1,7 @@
--- 7. Реализовать на языке Haskell функциию, 
--- заменменяющую в исходном списке все вхождения заданного значения другим..
+-- 7. Реализовать на языке Haskell функциию, заменменяющую в исходном списке все вхождения заданного значения другим..
+
 replacement :: [Integer] -> Integer -> Integer -> [Integer]
-
 replacement [] _ _ = []
-
 replacement (x:xs) a b = if a == x
                          then b:(replacement xs a b)
                          else x:(replacement xs a b)
