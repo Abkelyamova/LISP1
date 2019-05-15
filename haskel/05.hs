758e6f76661845f483f715974d811d363e998c69
Abkelyamova <43819580+Abkelyamova@users.noreply.github.com>
2019-05-15 22:41:41 +0300

    Update 05.hs

--- haskel/05.hs
+++ haskel/05.hs
@@ -2,7 +2,7 @@
 -- замененящую в исходном списке два подряд идущих одинаковых элемента одним
 
 delete_couple :: [Integer] -> [Integer]
-delete_couple []         = []
+delete_couple [] = []
 delete_couple (x:xs:xss) = if x == xs
                            then x:(delete_couple xss)
                            else x:(delete_couple (xs:xss))
