8e5c17ddc2bd9522e91fbbe084d2842ea385ba9a
Abkelyamova <43819580+Abkelyamova@users.noreply.github.com>
2019-05-15 19:23:45 +0300

    Update 04.hs

--- haskel/04.hs
+++ haskel/04.hs
@@ -4,9 +4,8 @@
 split_lists :: [Integer] -> ([Integer],[Integer])
 split_lists []     = ([], [])
 split_lists (x:xs) = if x > 0
-               then (x: first, second)
-               else (first, x: second)
-               where
-                   (first, second) = (split_lists xs)
+                     then (x: first, second)
+                     else (first, x: second)
+                     where (first, second) = (split_lists xs)
 
 main = print $ split_lists [-3..3] -- ([1,2,3],[-3,-2,-1,0])
