bbeba8e729e9746aada7e6a2dab1ebcf44cb6d01
Abkelyamova <43819580+Abkelyamova@users.noreply.github.com>
2019-05-15 19:07:46 +0300

    Create 03.hs

---
+++ haskel/03.hs
@@ -0,0 +1,12 @@
+--4. Реализовать на языке Haskell функцию, которая разделяет исходный список 
+--из целых чисел на два списка: список положителтных чисел и список отрицательных чисел.
+
+split_lists :: [Integer] -> ([Integer],[Integer])
+split_lists []     = ([], [])
+split_lists (x:xs) = if x > 0
+               then (x: first, second)
+               else (first, x: second)
+               where
+                   (first, second) = (split_lists xs)
+
+main = print $ split_lists [-3..3] -- ([1,2,3],[-3,-2,-1,0])
