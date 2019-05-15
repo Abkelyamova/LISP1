563d109277c0191793d239923db3aed86e938e08
Abkelyamova <43819580+Abkelyamova@users.noreply.github.com>
2019-05-15 22:39:00 +0300

    Update 11.hs

--- haskel/11.hs
+++ haskel/11.hs
@@ -3,4 +3,4 @@
 mixing_lists [] [] = []
 mixing_lists (x:xs) (y:ys) = x:y:(mixing_lists xs ys)
 
-main = print $ (mixing_lists [1, 3, 5] [2, 4, 6]) -- [1,2,3,4,5,6]
+main = print (mixing_lists [1, 3, 5] [2, 4, 6]) -- [1,2,3,4,5,6]
