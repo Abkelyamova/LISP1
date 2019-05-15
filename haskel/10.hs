da41be56dea914e353f3c77d9ea7b4f25320576c
Abkelyamova <43819580+Abkelyamova@users.noreply.github.com>
2019-05-15 22:53:18 +0300

    Rename 11.hs to 10.hs

---
+++ haskel/10.hs
@@ -0,0 +1,7 @@
+-- 10. Реализовать на языке Haskell функцию, вычисляющую скалярное проииведение векторов, заданных списками целых чисел.
+
+scalar :: [Integer] -> [Integer] -> Integer
+scalar [x] [y] = x*y
+scalar (x:xs) (y:ys) = x*y + (scalar xs ys)
+
+main = print (scalar   [1,2] [3,4]) -- 11
