323433d12765df52535c15b1fac0bab4cd2ef6af
Abkelyamova <43819580+Abkelyamova@users.noreply.github.com>
2019-05-16 09:19:07 +0400

    Update 01.hs

--- haskel/01.hs
+++ haskel/01.hs
@@ -1,5 +1,6 @@
 -- 1. Реализовать на языке Haskell функцию вычисления суммы элементов списка.
 
+sum_of_elements_list :: [Integer] -> Integer
 sum_of_elements_list [] = 0
 sum_of_elements_list (x:xs) = x + (sum_of_elements_list xs)
 
