659447321d6a9bc200e8851cb9c94879ac6e62b3
Abkelyamova <43819580+Abkelyamova@users.noreply.github.com>
2019-05-16 09:19:43 +0400

    Update 02.hs

--- haskel/02.hs
+++ haskel/02.hs
@@ -1,5 +1,6 @@
 -- 2. Реализовать на языке Haskell функцию нахождения максимального элемента списка. 
 
+max_element_in_line :: [Integer] -> Integer
 max_element_in_line [] = 0
 max_element_in_line (x:xs) = 
             if x > (max_element_in_line xs)
