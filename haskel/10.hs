e30e0963e29a1cd5dc929870925d288b0266f1e8
Abkelyamova <43819580+Abkelyamova@users.noreply.github.com>
2019-05-16 10:11:27 +0400

    Update 10.hs

--- haskel/10.hs
+++ haskel/10.hs
@@ -5,3 +5,10 @@ scalar [x] [y] = x*y
 scalar (x:xs) (y:ys) = x*y + (scalar xs ys)
 
 main = print (scalar   [1,2] [3,4]) -- 11
+
+-----------------------------------------
+
+scalar :: [Integer] -> [Integer] -> Integer
+scalar (x:xs) (y:ys) =  foldr (+) 0 (zipWith (*) (x:xs) (y:ys) )
+
+main = print (scalar [1,2] [3,4])
